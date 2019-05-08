39a928ec56bc12f77995cb95754a083d6a26cc0f
ryuuuuuu1 <ryu@ryunoMacBook-Air.local>
2019-05-08 21:13:28 +0900

    Add

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def edit
+  end
+  def update
+    if current_usr.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email,:encrypted_password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
-  
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
