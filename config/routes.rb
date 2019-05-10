635db02f4ba66cec93196c4a2f4c9d205e3973cb
ryuuuuuu1 <ryu@ryunoMacBook-Air.local>
2019-05-10 19:33:12 +0900

    dogs_info

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :groups
   resources :users,only: [:index]
+  resources :dogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
