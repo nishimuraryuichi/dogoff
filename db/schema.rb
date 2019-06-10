da9f07ab76d21567abe925c7647e7dbeb35dd206
ryuuuuuu1 <ryu@ryunoMacBook-Air.local>
2019-06-10 17:04:25 +0900

    Add change column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_041257) do
+ActiveRecord::Schema.define(version: 2019_06_10_072854) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
